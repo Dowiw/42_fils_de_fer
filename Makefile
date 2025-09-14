abffc0db27d20da3e8f60d1676d2f83a2f864480
Dowiw <monjie12272@gmail.com>
2025-09-15 01:00:47 +0200

    updating makefiles

--- Makefile
+++ Makefile
@@ -10,6 +10,47 @@
 #                                                                              #
 # **************************************************************************** #
 
-NAME=fdf
+NAME = fdf
+CC = gcc
+C_FLAGS = -Wall -Werror -Wextra
 
-COMPILE=gcc your_file.c -I./minilibx-linux -L./minilibx-linux -lmlx -lXext -lX11 -lm -lbsd
+SRC_DIR = ./src
+LIBFT = $(SRC_DIR)/libft/libft.a
+
+MLX_DIR = ./mlx
+MLX_FLAGS = -I$(MLX_DIR) -L$(MLX_DIR) -lmlx -lXext -lX11 -lm -lbsd
+
+C_FILES = main.c
+SRC = $(addprefix $(SRC_DIR)/, $(C_FILES))
+
+OBJ = $(SRC:.c=.o)
+
+INCLUDES = ./headers
+
+#  -- Rules start here --
+all: $(LIBFT) $(NAME)
+
+$(LIBFT):
+	make -C $(SRC_DIR)/libft/
+
+$(NAME): $(OBJ)
+	@$(CC) $(C_FLAGS) $(OBJ) $(LIBFT) -o $(NAME) $(MLX_FLAGS)
+
+%.o: %.c
+	@echo "Compiling $<..."
+	@$(CC) $(C_FLAGS) -c $< -o $@ -I$(INCLUDES) -I$(MLX_DIR)
+
+clean:
+	@make -C ./src/libft clean
+	@echo "Deleting object files in fdf..."
+	@rm -f $(OBJ)
+
+fclean: clean
+	@make -C ./src/libft fclean
+	@echo "Deleting all binaries in fdf..."
+	@rm -f $(NAME)
+	@rm -f $(OBJ)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- src/main.c
+++ src/main.c
@@ -12,13 +12,67 @@
 
 #include <stdio.h>
 
+#include "libft.h"
+
 #include <mlx.h>
 
 #include <math.h>
 
 #include <unistd.h>
 
+#include <limits.h>
+
 #include <stdlib.h>
+
+typedef struct s_point
+{
+	int	x;
+	int	y;
+	int	z;
+	int	color; // hex value in decimal
+}		t_point;
+
+typedef struct s_map
+{
+	int		width;
+	int		height;
+	t_point	**points;
+}			t_map;
+
+typedef struct s_mlx
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr;
+	char	*img_data;
+	int		bpp;
+	int		size_line;
+	int		endian;
+}			t_mlx;
+
+/**
+ * - Checks if av[1] is in valid .fdf format
+ * - Prints out respective errors
+ * - Filename should be below 255 by default
+ */
+static int	is_valid_format(char *filename)
+{
+	int	len;
+
+	len = 0;
+	while (filename[len] != '\0')
+		len++;
+	if (len > 255)
+		return (write(2, "Filename too large\n", 20), 0);
+	if (len < 4)
+		return (write(2, "Filename too short\n", 20), 0);
+	if (filename[len - 4] == '.'
+		&& filename[len - 3] == 'f'
+		&& filename[len - 2] == 'd'
+		&& filename[len - 1] == 'f')
+		return (1);
+	return (write(2, "Error: Filename is not in .fdf format\n", 39), 0);
+}
 
 /**
  * - Main (assumes maps are in correct format)
@@ -27,10 +81,10 @@ int main(int ac, char **av)
 {
 	void	*mlx_ptr;
 
-	if (ac != 2)
+	if (ac != 2 || !is_valid_format(av[1]))
 		return (write(2 ,"Usage: ./fdf <valid file in .fdf format>\n", 42), 1);
+	printf("arg: %s\n", av[1]);
 	mlx_ptr = mlx_init();
-
 	mlx_destroy_display(mlx_ptr); // very important frees for 0 leaks
 	free(mlx_ptr);
 }
