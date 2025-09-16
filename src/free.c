3e9bfdbcb4f2e71478818dc9417a1891d8ee22a1
Kabuñiang Buhawi (KB) Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-09-16 18:15:04 +0200

    add: free.c (file for freeing structs)
    This file contains functions to free allocated memory for the map structure and the entire data structure, ensuring proper memory management.

---
+++ src/free.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmonjard <kmonjard@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/16 12:11:55 by kmonjard          #+#    #+#             */
+/*   Updated: 2025/09/16 12:11:56 by kmonjard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fils_de_fer.h"
+
+/**
+ * - Frees the allocated points array inside map
+ */
+void	free_map(t_map *map)
+{
+	int	i;
+
+	i = 0;
+	if (map->points)
+	{
+		i = 0;
+		while (i < map->height)
+			free(map->points[i++]);
+		free(map->points);
+	}
+}
+
+/**
+ * - Frees the whole data structure
+ * - NOTE: only points** and the mlx library is allocated in heap memory
+ * 	everything else is in stack memory
+ */
+void	free_data(t_fdf_data *data)
+{
+	free_map(&data->map);
+	if (data->mlx.img_ptr)
+		mlx_destroy_image(data->mlx.mlx_ptr, data->mlx.img_ptr);
+	if (data->mlx.win_ptr)
+		mlx_destroy_window(data->mlx.mlx_ptr, data->mlx.win_ptr);
+	if (data->mlx.mlx_ptr)
+	{
+		mlx_destroy_display(data->mlx.mlx_ptr);
+		free(data->mlx.mlx_ptr);
+	}
+}
