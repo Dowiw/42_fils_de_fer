4aab74f01202572c5980bdee4523354c595b483e
Dowiw <monjie12272@gmail.com>
2025-09-19 00:30:18 +0200

    upd: events

--- src/events.c
+++ src/events.c
@@ -34,3 +34,12 @@ int	handle_close(t_fdf_data *data)
 	exit(EXIT_SUCCESS);
 	return (0);
 }
+
+/**
+ * - Initializer of events into the window
+ */
+void	init_mlx_events(t_fdf_data *data)
+{
+	mlx_key_hook(data->mlx.win_ptr, handle_key, data);
+	mlx_hook(data->mlx.win_ptr, EVENT_DESTROY, 0, handle_close, data);
+}
