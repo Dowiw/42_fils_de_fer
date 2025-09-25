d0b0506e47fb742b552750079a71e5a839fab19c
Kabuñiang Buhawi (KB) Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-09-25 19:38:02 +0200

    rm: key handling in events
    Removed key handling logic from handle_key function and replaced it with key_press and key_release hooks.

--- src/events.c
+++ src/events.c
@@ -13,37 +13,6 @@
 #include "fils_de_fer.h"
 
 /**
- * - Handle key inputs
- */
-static int	handle_key(int keycode, t_fdf_data *data)
-{
-	if (keycode == XK_Escape)
-	{
-		free_data(data);
-		exit(EXIT_FAILURE);
-	}
-	else if (keycode == XK_minus)
-		data->map.z_scale -= 0.1;
-	else if (keycode == XK_equal)
-		data->map.z_scale += 0.1;
-	else if (keycode == XK_Left)
-		data->map.rot_z -= 0.1;
-	else if (keycode == XK_Right)
-		data->map.rot_z += 0.1;
-	else if (keycode == XK_w)
-		data->map.rot_x += 0.1;
-	else if (keycode == XK_s)
-		data->map.rot_x -= 0.1;
-	else if (keycode == XK_a)
-		data->map.rot_y += 0.1;
-	else if (keycode == XK_d)
-		data->map.rot_y -= 0.1;
-	clear_image(&data->mlx);
-	draw_map(data);
-	return (0);
-}
-
-/**
  * - Handle close (the x button)
  */
 static int	handle_close(t_fdf_data *data)
@@ -53,6 +22,9 @@ static int	handle_close(t_fdf_data *data)
 	return (0);
 }
 
+/**
+ * - Handles minimizing and maximizing
+ */
 static int	expose_handler(t_fdf_data *data)
 {
 	clear_image(&data->mlx);
@@ -65,7 +37,9 @@ static int	expose_handler(t_fdf_data *data)
  */
 void	init_mlx_events(t_fdf_data *data)
 {
-	mlx_key_hook(data->mlx.win_ptr, handle_key, data);
 	mlx_hook(data->mlx.win_ptr, EVENT_DESTROY, 0, handle_close, data);
 	mlx_hook(data->mlx.win_ptr, EVENT_EXPOSE, 1L << 15, expose_handler, data);
+	mlx_hook(data->mlx.win_ptr, EVENT_KEY_PRESS, 1L << 0, key_press, data);
+	mlx_hook(data->mlx.win_ptr, EVENT_KEY_RELEASE, 1L << 1, key_release, data);
+	mlx_loop_hook(data->mlx.mlx_ptr, loop_hook, data);
 }
