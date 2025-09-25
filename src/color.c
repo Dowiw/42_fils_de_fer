9bb798f905d65b7859a234652814c487243e498c
Kabuñiang Buhawi (KB) Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-09-25 19:36:46 +0200

    add: color interpolation
    Implement color interpolation based on a ratio.

---
+++ src/color.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmonjard <kmonjard@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/25 10:35:50 by kmonjard          #+#    #+#             */
+/*   Updated: 2025/09/25 10:35:51 by kmonjard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * - Interpolate color based on the ratio value given
+ * - This link was super helpful:
+ *   https://stackoverflow.com/questions/13488957
+ */
+int	interpolate_color(int color1, int color2, double ratio)
+{
+	int	r;
+	int	g;
+	int	b;
+	int	color1_arr[3];
+	int	color2_arr[3];
+
+	if (ratio < 0.0)
+		ratio = 0.0;
+	if (ratio > 1.0)
+		ratio = 1.0;
+	color1_arr[0] = (color1 >> 16) & 0xFF;
+	color1_arr[1] = (color1 >> 8) & 0xFF;
+	color1_arr[2] = color1 & 0xFF;
+	color2_arr[0] = (color2 >> 16) & 0xFF;
+	color2_arr[1] = (color2 >> 8) & 0xFF;
+	color2_arr[2] = color2 & 0xFF;
+	r = (int)(color1_arr[0] + (color2_arr[0] - color1_arr[0]) * ratio);
+	g = (int)(color1_arr[1] + (color2_arr[1] - color1_arr[1]) * ratio);
+	b = (int)(color1_arr[2] + (color2_arr[2] - color1_arr[2]) * ratio);
+	return ((r << 16) | (g << 8) | b);
+}
