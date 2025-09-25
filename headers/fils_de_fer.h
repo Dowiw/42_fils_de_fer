ffd6c67b7ff392283d8c81809730a9c0f7e7f13d
Kabuñiang Buhawi (KB) Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-09-25 19:31:59 +0200

    upd: header
    Updated structures and added documentation for clarity.
    
    Refactor pixel and map structures with documentation.

--- headers/fils_de_fer.h
+++ headers/fils_de_fer.h
@@ -45,38 +45,64 @@ typedef enum e_event
 	EVENT_DESTROY = 17
 }	t_event;
 
-// structure to contain two pieces of data (x, y) of a pixel
+/**
+ * - Structure for a pixel
+ *
+ * @param x coordinate from 0 to the window width
+ * @param y coordiamte from 0 to the window height
+ * @param color color of the pixel
+ */
 typedef struct s_pixel
 {
 	int	x;
 	int	y;
+	int	color;
 }		t_pixel;
 
-// each point in the map
-typedef struct s_point
-{
-	int	x; // abscissa
-	int	y; // ordinate
-	int	z; // altitude
-	int	color; // hex value in decimal
-}		t_point;
-
-// structure to store point data and render values
+/**
+ * - Structure for the map, rotations, offsets, views
+ *
+ * @param width map's width
+ * @param height map's height
+ * @param z_arr array of z values from the map
+ * @param colors array of colors for each respective point
+ * @param offset_w offset from the width of the window
+ * @param offset_h offset from the height of the window
+ * @param view the initial viewing angle (set to M_PI / 6 by default)
+ * @param size the scale of which to render the map
+ * @param z_scale the magnitude of the z value
+ * @param rot_x angle of rotation for x
+ * @param rot_y angle of rotation for y
+ * @param rot_z angle of rotation for z
+ */
 typedef struct s_map
 {
+	int		orth_view;
 	int		width;
 	int		height;
 	int		**z_arr;
 	int		**colors;
-	double	angle;
-	double	map_scale;
-	double	z_scale; // scale of magnitude for z value
-	double	rot_x; // angle of rotation for X
-	double	rot_y; // angle of rotation for y
-	double	rot_z; // angle of rotation for z
+	double	offset_w;
+	double	offset_h;
+	double	view;
+	double	size;
+	double	z_scale;
+	double	rot_x;
+	double	rot_y;
+	double	rot_z;
 }			t_map;
 
-// structure for mlx data
+/**
+ * - Structure for mlx data
+ * - Panning variables might be unnecessary
+ * @param mlx_ptr pointer to the mlx library that allocates an XWindow display
+ * @param win_ptr pointer to the window allocated
+ * @param img_ptr pointer to the image buffer
+ * @param img_data pointer to the same img but as characters
+ * @param bpp bits per pixel
+ * @param size_line size of each line of pixels
+ * @param endian necessary for alignment
+ */
 typedef struct s_mlx
 {
 	void	*mlx_ptr;
@@ -86,6 +112,10 @@ typedef struct s_mlx
 	int		bpp;
 	int		size_line;
 	int		endian;
+	int		pan_left;
+	int		pan_right;
+	int		pan_up;
+	int		pan_down;
 }			t_mlx;
 
 // structure for fdf data
@@ -102,13 +132,30 @@ typedef struct s_fdf_data
 
 // allocate.c
 
-int	allocate_and_input(int file_fd, t_map *map);
+int		allocate_and_input(int file_fd, t_map *map);
+void	put_datapoint(t_map *map, char *token, int row, int col);
 
-// calculate.c
+// calc_angles.c
 
 t_pixel	calc_iso(t_map *map_data, int x, int y, int z);
-void	draw_bresenham_line(t_mlx *mlx, t_pixel start, t_pixel end, int color);
-void	init_angles(t_map *map);
+
+// calc_line.c
+
+void	draw_bresenham_line(t_mlx *mlx, t_pixel start, t_pixel end);
+
+// calc_size.c
+
+double	calc_size(t_map *map);
+
+// color.c
+
+int		interpolate_color(int color1, int color2, double ratio);
+
+// events_keys,c
+
+int		key_release(int keycode, t_fdf_data *data);
+int		key_press(int keycode, t_fdf_data *data);
+int		loop_hook(t_fdf_data *data);
 
 // events.c
 
@@ -127,15 +174,21 @@ void	put_pixel(t_mlx *mlx, int x, int y, int color);
 
 // initializers.c
 
-void	put_datapoint(t_map *map, char *token, int row, int col);
 int		init_mlx_lib(t_mlx *mlx, t_fdf_data *data);
 void	init_map(t_map *map);
 void	init_mlx(t_mlx *mlx);
+void	init_angles(t_map *map);
 
 // parse_map.c
 
-int	is_valid_format(char *filepath);
-int	input_points(int file_fd, t_map *map);
-int	parse_map(char *filepath, t_map *map);
+int		is_valid_format(char *filepath);
+int		input_points(int file_fd, t_map *map);
+int		parse_map(char *filepath, t_map *map);
+
+// redraw.c
+
+void	redraw_iso(t_fdf_data *data);
+void	draw_plan_view(t_fdf_data *data);
+void	centralize(t_fdf_data *data);
 
 #endif
